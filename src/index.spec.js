c21c1f92feff805ea84f0ab4b4d04791758442c7
Peter Lin <plin@petroiq.com>
2021-11-30 10:52:53 -1000

    test

--- src/index.spec.js
+++ src/index.spec.js
@@ -13,13 +13,13 @@ describe("CRLF Converter", () => {
     it("should replace CRLF at the end of a string", () => {
       const result = lf`Hello there.\r\n`;
 
-      // Use `String.prototype.endsWith()`
+      assert(result.endsWith('there.\n'))
     });
 
     it("should replace CRLF in the middle of a string", () => {
       const result = lf`Hello \r\n\r\nthere.`;
 
-      // Use `String.prototype.includes()`
+      assert(result.includes('\n'))
     });
   });
 });
