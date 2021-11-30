b008e233941af8a3bad5caf98e79eccc396d0159
Peter Lin <plin@petroiq.com>
2021-11-30 09:30:45 -1000

    3

--- src/info.js
+++ src/info.js
@@ -2,25 +2,18 @@ const usage = "Usage: crlf-convert [CR|LF|CRLF]";
 
 const help = `Usage: crlf-convert [CR|LF|CRLF]
 
-
          Converts text between newline (\\n), carriage return (\\r), and combined
          (\\r\\n) line endings.
          
-
          Options:
-
 
          [CR|LF|CRLF]: Desired line ending
 
-
              CR is a carriage return character \\r (rare)
-
              LF is a line feed character \\n (common on Unix and macOS)
-
              CRLF is a combined ending \\r\\n (common on Windows)
-
-         Example:
-
+         
+             Example:
 
               cat in.txt | crlf-convert LF > out.txt`;
 
